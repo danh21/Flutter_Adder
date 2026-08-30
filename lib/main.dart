9facf15e644e5a4793ae232214212fb55eacd52d
Danh Phan <phandanh2112@gmail.com>
2026-08-30 10:49:24 +0700

    update: rename app

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Calculator',
+      title: 'calculator',
       theme: new ThemeData(
         primarySwatch: Colors.blue,
       ),
